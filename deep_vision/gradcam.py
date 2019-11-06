d7a0f2f34a38af897d646b79eb273e45137cdbe6
sukant <sukant.iitr@gmail.com>
2019-11-06 09:58:19 +0530

    added files for assignment 13

--- deep_vision/gradcam.py
+++ deep_vision/gradcam.py
@@ -22,7 +22,8 @@ def gradcam(actual_image, normalised_image, model, layer, n_channels):
 
     grads = K.gradients(class_output, last_conv_layer.output)[0]
     pooled_grads = K.mean(grads, axis=(0, 1, 2))
-    iterate = K.function([model.input], [pooled_grads, last_conv_layer.output[0]])
+    iterate = K.function(
+        [model.input], [pooled_grads, last_conv_layer.output[0]])
     pooled_grads_value, conv_layer_output_value = iterate([x])
     for j in range(n_channels):
         conv_layer_output_value[:, :, j] *= pooled_grads_value[j]
@@ -31,7 +32,8 @@ def gradcam(actual_image, normalised_image, model, layer, n_channels):
     heatmap = np.maximum(heatmap, 0)
     heatmap /= np.max(heatmap)
 
-    heatmap = cv2.resize(heatmap, (normalised_image.shape[1], normalised_image.shape[0]))
+    heatmap = cv2.resize(
+        heatmap, (normalised_image.shape[1], normalised_image.shape[0]))
     heatmap = np.uint8(255 * heatmap)
     heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
     superimposed_img = cv2.addWeighted(actual_image, 0.6, heatmap, 0.4, 0)
